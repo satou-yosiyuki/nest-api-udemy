952d5f2a0416e3ac7edb5223a396b588911e81be
satou-yosiyuki <stuysyk1027@icloud.com>
2022-09-07 08:58:21 +0900

    各モジュールの生成

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { TodoModule } from './todo/todo.module';
+import { PrismaService } from './prisma/prisma.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    AuthModule,
+    UserModule,
+    TodoModule,
+    PrismaModule,
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,21 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { request } from 'express';
 import { AppModule } from './app.module';
+import * as cookieParser from 'cookie-parser';
+import * as csurf from 'csurf';
 
 // バックエンドは3005
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  app.enableCors({
+    // フロント、バックをcookieベースで通信する
+    credentials: true,
+    // React側のアクセスを許可
+    origin: ['http://localhost:3000'],
+  });
+  app.use(cookieParser());
   await app.listen(3005);
 }
 bootstrap();
