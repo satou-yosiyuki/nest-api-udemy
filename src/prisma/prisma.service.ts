04dd9b5a42e3d404d1a706de69b9039f7ed70c45
satou-yosiyuki <stuysyk1027@icloud.com>
2022-09-09 09:06:48 +0900

    authの実装

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -3,6 +3,7 @@ import { ConfigService } from '@nestjs/config';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
+// superを使って、PrismaClientのconstructorの処理を参照する
 export class PrismaService extends PrismaClient {
   constructor(private readonly config: ConfigService) {
     super({
